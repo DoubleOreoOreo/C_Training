4e84ad23af8a61e79fc6f8bf92ba7cb95ef3599d
doubleoreooreo <doubleoreooreo@gmail.com>
2021-06-26 18:41:11 +0800

    Add Push back and print function

--- LinkedList/LinkedList.c
+++ LinkedList/LinkedList.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 
 typedef struct ListNode ListNode;     //Forward declaration
-//ListNode* head = NULL;              //Head of Linkedlist (Global)
+ListNode* head = NULL;              //Head of Linkedlist (Global)
 
 struct ListNode{
     int val;
@@ -11,28 +11,55 @@ struct ListNode{
 
 ListNode* creatNode(int x)
 {
-    ListNode *new_node = NULL;
-    new_node = (struct ListNode*)malloc(sizeof(struct ListNode));
-
+    ListNode* new_node = (ListNode*)malloc(sizeof(ListNode));
     new_node->val = x;
     new_node->next = NULL;
-
+    
     return new_node;
 }
 
-void printList(struct ListNode* head)
+void printList()
 {
-    while (head != NULL)
-    {
-        printf("%d ", head->val);
-        head = head->next;
+    if(head == NULL){
+        printf("Linked list is empty");
+    }else{
+        ListNode *current = head;
+        printf("Linked list: ");
+        while (current != NULL)
+        {
+            printf("%d -> ", current->val);
+            current = current->next;
+        }
+        printf("NULL\n");
     }
+}
+
+void push_back(int x)
+{
+    ListNode *new_node = creatNode(x);
+    
+    if (head == NULL){
+        head = new_node;
+        printf("Success!\n");
+    }else{
+        ListNode *current = head;
+        
+        while(current->next != NULL){
+            current = current->next;
+        }
+        current->next = new_node;
+        printf("Success!\n");
+    }
+}
+
+void push_front(int x)
+{
+    
 }
 
 void insertAfterList(struct ListNode* ptr, int x)
 {
-    ListNode* new_node;
-    new_node = creatNode(x);
+    ListNode* new_node = creatNode(x);
 
     new_node->next = ptr->next;
     ptr->next = new_node;
@@ -57,21 +84,51 @@ void deleteList(struct ListNode* n, int x)
 
 int main()
 {
-    ListNode* head = NULL;
-    ListNode* second = NULL;
-    ListNode* third = NULL;
+    int option = 0;
+    int val = 0;
+    int flag = 1;
+    printf("(1) Push back\n");
+    printf("(2) Push front\n");
+    printf("(3) Insert x (Node: head -> x)\n");
+    printf("(4) Delete\n");
+    printf("(5) Print this link\n");
+    printf("(6) Quit\n");
+    while (flag){
+        printf("Enter your option: ");
+        scanf("%d", &option);
+        switch(option)
+        {
+            case 1: //Push one node at the end of the linked list
+                printf("Enter node value: ");
+                scanf("%d", &val);
+                push_back(val);
+                break;
+            case 2: //Push one node at the front of the linked list
 
-    head = creatNode(0);
-    second = creatNode(1);
-    third = creatNode(2);
+                break;
+            case 3: //Insert
 
-    head->next = second;
-    second->next = third;
+                break;
+            case 4: //Delete
 
-    insertAfterList(third, 10);
-    insertBeforeheadList(&head, 11);
+                break;
+            case 5: //Print
+                printList();
+                break;
+            case 6: //Exit
+                flag = 0;
+                break;
+            default:
+                break;
+        }
+    }
+    //second = creatNode(1);
+    //third = creatNode(2);
+
+    //head->next = second;
+    //second->next = third;
+    //insertBeforeheadList(&head, 11);
     
-    printList(head);
-    free(head);
+   free(head);
     return 0;
 }
