c2a55526fb15a2c240e988a4e12dd144ba5db760
doubleoreooreo <doubleoreooreo@gmail.com>
2021-06-29 09:17:34 +0800

    Update 0629

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# C
- C training
+# C/C++
+ C/C++ training
---
+++ leetcode/264_UglyNumberII.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<cstdlib>
+#include<algorithm>
+#include<vector>
+#include<cmath>
+using namespace std;
+
+int nthUglyNumber(int n){
+    int ans = 0;
+    int x, y, z;
+    int i = 0, j = 0, k = 0;
+    vector<int> ugly;
+    ugly.reserve(n);
+    ugly.push_back(1);
+
+    while(ugly.size() < n){
+        x = 2 * ugly[i];
+        y = 3 * ugly[j];
+        z = 5 * ugly[k];
+        ans = min(min(x, y), z);
+        i = (ans == x) ? ++i: i;
+        j = (ans == y) ? ++j: j;
+        k = (ans == z) ? ++k: k;
+        ugly.push_back(ans);
+    }
+
+    for (int i = 0; i<ugly.size(); i++)
+        cout << ugly[i] << " ";
+
+    return ugly.back();
+}
+
+int main()
+{
+    
+    printf("ans = %d\n", nthUglyNumber(11));
+    /*int* array = (int*)malloc(sizeof(int));
+    array[0] = 5;
+    array[1] = 2;
+
+    printf("%d\n", array[0]);
+    printf("%d\n", array[1]);*/
+    return 0;
+}
